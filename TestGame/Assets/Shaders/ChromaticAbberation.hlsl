89079a378744c7ec3f4eae26cb905503da326355
Zen <zenn.luu@gmail.com>
2024-06-18 19:46:06 +0300

    Chromatic abberation post process improvement

--- TestGame/Assets/Shaders/ChromaticAbberation.hlsl
+++ TestGame/Assets/Shaders/ChromaticAbberation.hlsl
@@ -20,15 +20,24 @@ float4 psmain(VPS_INOUT input) : SV_TARGET
     float4 color;
     float2 uv = input.texcoord.xy;
     
+    float2 pos = float2((uv.x - 0.5) * 2.0, (uv.y - 0.5) * 2);
+    float l = length(pos);
     
     float red_shift = 0.009;
-    float green_shift = 0.006;
-    float blue_shift = -0.006;
+    float green_shift = 0.004;
+    float blue_shift = -0.007;
     
-    color.r = Color.Sample(ColorSampler, uv + ((sin(time) + red_shift) * red_shift)).r;
-    color.g = Color.Sample(ColorSampler, uv + ((sin(time) + green_shift) * green_shift)).g;
-    color.b = Color.Sample(ColorSampler, uv + ((sin(time) + blue_shift) * blue_shift)).b;
+    //color.r = Color.Sample(ColorSampler, uv + ((sin(time) + red_shift) * red_shift)).r;
+    //color.g = Color.Sample(ColorSampler, uv + ((sin(time) + green_shift) * green_shift)).g;
+    //color.b = Color.Sample(ColorSampler, uv + ((sin(time) + blue_shift) * blue_shift)).b;
+    //color.a = 1.0;
+    
+    color.r = Color.Sample(ColorSampler, uv + l * red_shift).r;
+    color.g = Color.Sample(ColorSampler, uv + l * green_shift).g;
+    color.b = Color.Sample(ColorSampler, uv + l * blue_shift).b;
     color.a = 1.0;
     
+    
     return color;
+
 }
